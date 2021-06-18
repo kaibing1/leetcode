4c80def75ae85b3fd43f444f71c6199da055a97d
kaibing1 <yang1148436163@163.com>
2021-06-18 09:27:35 +0900

    add binary search

--- src/main/java/com/kb/d17/Solution.java
+++ src/main/java/com/kb/d17/Solution.java
@@ -10,6 +10,52 @@ import java.util.Map;
  * @Date: Created in 20:38 2021/6/17
  */
 public class Solution {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 3, 3, 4, 5};
+        int target = 3;
+        System.out.println(rightBound(nums, target));
+    }
+
+    public static int leftBound(int[] nums, int target){
+        if (nums.length == 0) return -1;
+        int left = 0, right = nums.length-1;
+        while (left <= right){
+            int mid = left + (right-left)/2;
+            if (nums[mid] == target){
+                right = mid-1;
+            }else if (nums[mid] < target){
+                left = mid + 1;
+            }else if (nums[mid] > target){
+                right = mid-1;
+            }
+        }
+        if (left>= nums.length || nums[left] != target){
+            return -1;
+        }
+        return left;
+    }
+
+    public static int rightBound(int[] nums, int target){
+        int left = 0, right = nums.length -1;
+        while (left <= right){
+            int mid = left + (right-left);
+            if (nums[mid] == target){
+                left = mid + 1;
+            }else if (nums[mid] > target){
+                right = mid -1;
+            }else if (nums[mid] < target){
+                left = mid + 1;
+            }
+        }
+        if (right<0 || nums[right] != target){
+            return -1;
+        }
+        return right;
+    }
+
+
+
     Map<Character, Integer> ori = new HashMap<>();
     Map<Character, Integer> cnt = new HashMap<>();
     // 76
