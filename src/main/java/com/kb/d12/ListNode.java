506d57ec57f89aea9e18e122f2c943ed8daaaa92
kaibing1 <yang1148436163@163.com>
2021-07-29 16:23:38 +0900

    add the solution

--- src/main/java/com/kb/d12/ListNode.java
+++ src/main/java/com/kb/d12/ListNode.java
@@ -3,8 +3,16 @@ package com.kb.d12;
 public class ListNode {
     int val;
     ListNode next;
-    ListNode(int x){
-        val = x;
-        next = null;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
